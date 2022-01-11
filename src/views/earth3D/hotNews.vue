580c16e008924a61350b0b32def70623f5c772e6
liuyang <liuyang@ktwlrj.com>
2022-01-11 10:05:03 +0800

    .

--- src/views/earth3D/hotNews.vue
+++ src/views/earth3D/hotNews.vue
@@ -35,6 +35,18 @@
         },
         mounted() {
             this.init()
+
+             // 自适应 onresize 事件会在窗口被调整大小时发生
+             window.onresize = function () {
+                // 重置渲染器输出画布canvas尺寸
+                renderer.setSize(container.clientWidth, container.clientHeight);
+                // 全屏情况下：设置观察范围长宽比aspect为窗口宽高比
+                camera.aspect = container.clientWidth / container.clientHeight;
+                // 渲染器执行render方法的时候会读取相机对象的投影矩阵属性projectionMatrix
+                // 但是不会每渲染一帧，就通过相机的属性计算投影矩阵(节约计算资源)
+                // 如果相机的一些属性发生了变化，需要执行updateProjectionMatrix ()方法更新相机的投影矩阵
+                camera.updateProjectionMatrix();
+            };
         },
         methods: {
             async init() {
